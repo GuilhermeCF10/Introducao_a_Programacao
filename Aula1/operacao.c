0ed02565525cb4f093ac614cb95e9ab790fa010f
Guilherme Fialho <guilhermec.fialho@gmail.com>
2022-06-24 11:12:33 -0300

    First Commit

---
+++ Aula1/operacao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, d, operacao;
+    a = 2;
+    b = 5;
+    c = 1;
+    d = 2;
+    operacao = a + b - c + d;
+    printf("O valor da operacao eh:\n%d", operacao);
+
+    return 0;
+}

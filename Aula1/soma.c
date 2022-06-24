e8d2461952d6f2b969d3c155ede2ec470ae8dbab
Guilherme Fialho <guilhermec.fialho@gmail.com>
2022-06-24 11:07:50 -0300

    First Commit

--- Aula1/soma.c
+++ Aula1/soma.c
@@ -3,13 +3,10 @@
 int main()
 {
     int a, b, soma;
-    printf("Digite o primeiro inteiro\n");
-    scanf("%d", &a);
-    printf("Digite o segundo inteiro\n");
-    scanf("%d", &b);
+    a = 20;
+    b = 40;
     soma = a + b;
     printf("O valor da soma eh:\n%d", soma);
-
 
     return 0;
 }
---
+++ Aula1/subtracao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, subtracao;
+    a = 20;
+    b = 40;
+    subtracao = a - b;
+    printf("O valor da subtracao eh:\n%d", subtracao);
+
+    return 0;
+}
